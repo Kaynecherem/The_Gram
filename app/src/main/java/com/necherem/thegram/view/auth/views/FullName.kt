0d13aa086d397d1ae26f0d58a96cfcc6e1e182b1
Kalu Chinecherem <kaluchinecherem8@gmail.com>
2024-01-24 21:37:29 +0100

    Date and time picker

---
+++ app/src/main/java/com/necherem/thegram/view/auth/views/FullName.kt
@@ -0,0 +1,106 @@
+package com.necherem.thegram.view.auth.views
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeDrawingPadding
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.necherem.thegram.R
+import com.necherem.thegram.elements.DisplayText
+import com.necherem.thegram.elements.UserFilledButton
+import com.necherem.thegram.elements.UserInputField
+import com.necherem.thegram.elements.UserTextButton
+
+@Composable
+fun FullName(
+    modifier: Modifier = Modifier
+) {
+    var fullName by remember{ mutableStateOf("") }
+    Column(
+        modifier = Modifier
+            .background(Color.Transparent)
+            .fillMaxSize()
+            .statusBarsPadding()
+            .padding(top = 24.dp)
+            .verticalScroll(rememberScrollState())
+            .safeDrawingPadding(),
+        verticalArrangement = Arrangement.SpaceBetween
+
+    ) {
+        Column(
+            modifier = modifier
+                .fillMaxWidth(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.spacedBy(space = 8.dp)
+        ) {
+            DisplayText(
+                text = "What is your name?",
+                color = MaterialTheme.colorScheme.onBackground,
+                modifier = modifier
+                    .align(Alignment.Start),
+                fontSize = 24.sp,
+                fontWeight = FontWeight.Bold
+            )
+            UserInputField(
+                label = R.string.full_name,
+                keyboardOptions = KeyboardOptions(
+                    keyboardType = KeyboardType.Email,
+                    imeAction = ImeAction.Next
+                ),
+                value = fullName,
+                onValueChange = { fullName = it },
+                modifier = Modifier
+                    .padding(bottom = 8.dp)
+                    .fillMaxWidth()
+            )
+            UserFilledButton(
+                onValueChange = { /*TODO*/ },
+                text = "Next",
+                modifier = modifier
+                    .fillMaxWidth(),
+            )
+        }
+        Column(
+            modifier = modifier
+                .fillMaxWidth()
+                .padding(all = 12.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.spacedBy(space = 8.dp)
+        ) {
+            UserTextButton(
+                onValueChange = { /*TODO*/ },
+                text = "Create new account",
+                textColor = MaterialTheme.colorScheme.primary,
+                modifier = modifier
+                    .fillMaxWidth()
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+fun FullNamePreview() {
+    FullName()
+}
